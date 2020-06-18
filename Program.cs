5a31f6a0aa7cfd1f904ce35768e0e6d02b0eaac8
krk1990 <67090044+krk1990@users.noreply.github.com>
2020-06-18 21:30:08 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareFreeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long inputedNumber=0;
+            int counter = 0;
+            Console.WriteLine("Enter a number and then press Enter");
+            inputedNumber = Convert.ToInt64(Console.ReadLine());
+            for (long divisorNumber = 2; divisorNumber <= inputedNumber; ++divisorNumber)
+            {
+                if (inputedNumber % divisorNumber == 0)
+                {
+                    counter++;
+                    //Console.Write(divisorNumber + "\t");
+                    while (inputedNumber % (divisorNumber * divisorNumber) == 0)
+                    {
+                        inputedNumber /= divisorNumber;
+                        
+                    }
+                }
+              
+            }
+            Console.Write(counter);
+            Console.ReadLine();
+        }
+    }
+}
